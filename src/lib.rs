8e19ab4870f0e88809bdcf873af902127f2d0199
Alex Zywicki <alexander.zywicki@gmail.com>
2017-11-15 20:42:12 -0500

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,25 @@
 extern crate num;
 use num::{Float, clamp, One, Zero, NumCast};
 
+fn nonzero<F>(value: F) -> F
+where
+    F: Float,
+{
+    use std::f64::{MIN_POSITIVE, MAX};
+    clamp(
+        value,
+        NumCast::from(MIN_POSITIVE).unwrap(),
+        NumCast::from(MAX).unwrap(),
+    )
+}
+
+fn normal<F>(value: F) -> F
+where
+    F: Float,
+{
+    clamp(value, Zero::zero(), One::one())
+}
+
 #[derive(Debug)]
 enum AdsrState {
     Ready,
@@ -39,17 +58,15 @@ where
     F: Float,
 {
     pub fn new(attack: F, decay: F, sustain: F, release: F, initial: F, peak: F, end: F) -> Self {
-        let zero = Zero::zero();
-        let one = One::one();
-
+        let zero: F = Zero::zero();
         Envelope {
             attack: nonzero(attack),
             decay: nonzero(decay),
-            sustain: clamp(sustain, zero, one),
+            sustain: normal(sustain),
             release: nonzero(release),
-            initial: clamp(initial, zero, one),
-            peak: clamp(peak, zero, one),
-            end: clamp(end, zero, one),
+            initial: normal(initial),
+            peak: normal(peak),
+            end: normal(end),
             state: AdsrState::Ready,
             slope: zero,
             value: initial,
@@ -59,21 +76,25 @@ where
             release_slope: zero,
         }
     }
+
     pub fn reset(&mut self) {
         self.slope = Zero::zero();
         self.value = self.initial;
         self.note_is_on = false;
         self.state = AdsrState::Ready;
     }
+
     pub fn note_on(&mut self) {
         self.reset();
         self.note_is_on = true;
     }
+
     pub fn note_off(&mut self) {
         self.note_is_on = false;
         self.state = AdsrState::Release;
         self.slope = self.calc_release_slope();
     }
+
     pub fn set_attack(&mut self, value: F) {
         self.attack = nonzero(value);
         self.attack_slope = self.calc_attack_slope();
@@ -81,6 +102,7 @@ where
             self.slope = self.attack_slope;
         }
     }
+
     pub fn set_decay(&mut self, value: F) {
         self.decay = nonzero(value);
         self.decay_slope = self.calc_decay_slope();
@@ -88,9 +110,11 @@ where
             self.slope = self.decay_slope;
         }
     }
+
     pub fn set_sustain(&mut self, value: F) {
-        self.sustain = clamp(value, Zero::zero(), One::one());
+        self.sustain = normal(value);
     }
+
     pub fn set_release(&mut self, value: F) {
         self.release = nonzero(value);
         self.release_slope = self.calc_release_slope();
@@ -100,19 +124,21 @@ where
     }
 
     pub fn set_initial(&mut self, value: F) {
-        self.initial = clamp(value, Zero::zero(), One::one());
+        self.initial = normal(value);
         if let AdsrState::Attack = self.state {
             self.attack_slope = self.calc_attack_slope();
         }
     }
+
     pub fn set_peak(&mut self, value: F) {
-        self.peak = clamp(value, Zero::zero(), One::one());
+        self.peak = normal(value);
         if let AdsrState::Decay = self.state {
             self.decay_slope = self.calc_decay_slope();
         }
     }
+
     pub fn set_end(&mut self, value: F) {
-        self.end = clamp(value, Zero::zero(), One::one());
+        self.end = normal(value);
         if let AdsrState::Release = self.state {
             self.release_slope = self.calc_release_slope();
         }
@@ -135,15 +161,6 @@ where
     fn calc_release_slope(&mut self) -> F {
         (self.end - self.sustain) / self.release
     }
-}
-
-fn nonzero<F: Float>(value: F) -> F {
-    use std::f64::{MIN_POSITIVE, MAX};
-    clamp(
-        value,
-        NumCast::from(MIN_POSITIVE).unwrap(),
-        NumCast::from(MAX).unwrap(),
-    )
 }
 
 impl<F> Iterator for Envelope<F>
@@ -194,6 +211,7 @@ where
             }
             AdsrState::Done => self.reset(),
         }
+
         let mut out = None;
         if let AdsrState::Release = self.state {
             out = Some(self.value);
